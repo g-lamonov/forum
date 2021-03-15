06031ff87e4cd320f77d59b926854d33dab5ff1b
g-lamonov <owsrey@gmail.com>
2021-03-15 23:54:30 +0700

    Add structure for sequelize and test

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 let express = require('express')
 let app = express()
 
-let config = require('./config.js')
+let config = require('./config/config.json')
+
 
 //Middle-ware
 let bodyParser = require('body-parser')
 let morgan = require('morgan')
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 if(process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'production') {
 	app.use(morgan('dev'))
 }
+
+app.use('/api/v1/user', require('./routes/user'))
 
 app.listen(config.port, () => {
 	console.log('Listening on ' + config.port)
---
+++ test/user.js
@@ -0,0 +1,30 @@
+process.env.NODE_ENV = 'test'
+
+let chai = require('chai')
+let chaiHttp = require('chai-http')
+let server = require('../index')
+let should = chai.should()
+
+chai.use(chaiHttp)
+
+describe('User', () => {
+	describe('/POST user', () => {
+		it('should create an account', (done) => {
+			chai.request(server)
+				.post('/api/v1/user')
+				.set('content-type', 'application/x-www-form-urlencoded')
+				.send({
+					username: 'test',
+					password: 'pass'
+				})
+				.end((err, res) => {
+					res.should.have.status(200)
+					res.should.be.json
+					res.body.should.have.property('username', 'test')
+					res.body.should.have.property('hash')
+					
+					done()
+				})
+		})
+	})
+})
