8a013d73d250a0f7250aa9c112bcdbdc4d7a9074
g-lamonov <owsrey@gmail.com>
2021-04-08 22:36:51 +0700

    Add user page | Create and use ThreadPost component for thead page | Get comments for user by passing comments query to route | Add thread-post component and get posts from server from user | On click of avatar icon go to user page | Add thread title to post optionally and on click navigate to post | Add pagination functionality to thread

---
+++ forum-frontend/src/components/routes/User.vue
@@ -0,0 +1,104 @@
+<template>
+	<div class='route_container'>
+		<div class='user_header'>
+			<div
+				class='user_header__icon'
+				:style='{ "background-color": (user || {}).color }'
+			>
+				{{username[0]}}
+			</div>
+			<div class='user_header__info'>
+				<span class='user_header__username'>{{username}}</span>
+				<span class='user_header__date' v-if='user'>Created: {{user.createdAt | formatDate('date') }}</span>
+			</div>
+			<div></div>
+		</div>
+		<div class='user_description' v-if='user' v-html='user.description'>
+		</div>
+		<div class='user_posts' :class='{ "user_posts--no_border_bottom": !posts.length }'>
+			<div class='user_posts__title'>Posts by username</div>
+			<thread-post v-for='post in posts' :key='post' :post='post' :show-thread='true'></thread-post>
+			<template v-if='!posts.length'>This user hasn't posted anything yet</template>
+		</div>
+	</div>
+</template>
+
+<script>
+	import ThreadPost from '../ThreadPost'
+	import AjaxErrorHandler from '../../assets/js/errorHandler'
+	export default {
+		name: 'user',
+		components: {
+			ThreadPost
+		},
+		data () {
+			return {
+				username: this.$route.params.username,
+				user: null,
+				posts: []
+			}
+		},
+		created () {
+			this.axios
+				.get(`/api/v1/user/${this.$route.params.username}?posts=true`)
+				.then(res => {
+					this.user = res.data
+					this.posts = res.data.Posts
+				})
+				.catch(AjaxErrorHandler(this.$store))
+		}
+	}
+</script>
+
+<style lang='scss' scoped>
+	@import '../../assets/scss/variables.scss';
+	.user_header {
+		display: flex;
+		align-items: center;
+		margin-bottom: 1.5rem;
+		@at-root #{&}__icon {
+			height: 4rem;
+			width: 4rem;
+			line-height: 4rem;
+			@include text($font--role-emphasis, 3rem);
+			text-align: center;
+			border-radius: 100%;
+			background-color: $color__gray--darkest;
+			color: #fff;
+		}
+		@at-root #{&}__info {
+			display: flex;
+			flex-direction: column;
+			margin-left: 1rem;
+			height: 4rem;
+		}
+		@at-root #{&}__username {
+			margin-top: -0.25rem;
+			font-size: 2rem;
+			font-weight: bold
+		}
+		@at-root #{&}__date {
+			color: $color__gray--darkest;
+			font-size: 1.5rem;
+		}
+	}
+	.user_description {
+		margin-left: 5rem;
+		width: 75%;
+	}
+	.user_posts {
+		width: calc(75% + 5rem);
+		&:last-child {
+			border-bottom: thin solid $color__gray--primary;
+		}
+		@at-root #{&}--no_border_bottom {
+			&:last-child {
+				border-bottom: none;
+			}
+		}
+		@at-root #{&}__title {
+			font-size: 1.5rem;
+			margin-bottom: 1rem;
+		}
+	}
+</style>
